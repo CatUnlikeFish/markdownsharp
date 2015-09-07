9863f2b802ed26092c06fa202d034f892a329707
Sergey Azarkevich <azarkevich@gmail.com>
2015-09-07 10:10:39 +0300

    Add processor - console tool for convert md to html

---
+++ MarkdownSharpProcessor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using MarkdownSharp;
+
+namespace MarkdownSharpProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var mdFile in args)
+            {
+                // Create new markdown instance
+                var mark = new Markdown();
+
+                // Run parser
+                var html = mark.Transform(File.ReadAllText(mdFile));
+
+                var htmlFile = Path.ChangeExtension(mdFile, ".html");
+
+                File.WriteAllText(htmlFile, html);
+            }
+        }
+    }
+}
